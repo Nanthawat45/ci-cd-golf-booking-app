a77eb65636d525494b6ff4e228b2fc31762fc589
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2025-04-10 11:39:09 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ci-cd-project/",
+  base: "/ci-cd-golf-booking-app/",
   build: {
     outDir: "dist",
   },
